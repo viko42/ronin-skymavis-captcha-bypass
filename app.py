a43f2326dc09e1ca4107ec6d84420f2c66401adb
Viko <18552774+viko42@users.noreply.github.com>
2024-10-02 00:20:09 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from solve_captcha import main
+import requests
+import logging
+
+app = Flask(__name__)
+
+proxies = {
+    'http': "--your-proxy--url",
+    'https': "--your-proxy--url",
+}
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+@app.route('/solve_captcha', methods=['POST'])
+def solve_captcha():
+    try:
+        id, token, index = main()
+        return jsonify({"result": {
+            "id": id,
+            "success": "right"
+        }, "token": token, "clicks": index }), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
